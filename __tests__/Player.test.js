a8a05eddbca4f2eb1567c2ac3f5ad7760dddbcc3
Danielle Fortin <fortind6@gmail.com>
2022-01-26 21:24:33 -0500

    added more objects to player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,4 +31,35 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//updating value of our player health halfway through the test to check for both true and false conditions
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+// call reduce health twice with super high value to make sure it never runs negative
+test("subtracts from new player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
